373e13f041fc0190675aa922331e9cba2c8bec3a
germantitov97 <germangood2@gmail.com>
2026-03-17 02:10:02 +0200

    up

--- _1_buisness_card.py
+++ _1_buisness_card.py
@@ -1,12 +1,12 @@
 name = input("Enter your name: ")
 email = input("Enter your email: ")
-phone_number = input("Enter your phone number: ")
+phone_type = input("Enter your phone number: ")
 job_title = input("Enter your job title: ")
 
 user_info = {
     "name": name,
     "email": email,
-    "phone_number": phone_number,
+    "phone_type": phone_type,
     "job_title": job_title
 }
 
@@ -15,5 +15,5 @@ print()
 print("--- Buisness Card ---")
 print(f"Name: {user_info['name']}")
 print("Email:", user_info['email'])
-print(f"Phone number: {user_info['phone_number']}")
+print(f"Phone type: {user_info['phone_type']}")
 print(f"Job title: {user_info['job_title']}")
